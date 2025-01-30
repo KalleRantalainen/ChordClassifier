640180635c56578f63c99bc0a0df378b9f98719d
Kalle Rantalainen <kllrantalainen366@gmail.com>
2025-01-30 09:58:45 +0200

    added gitignore and some data processing functions

--- model/file_io.py
+++ model/file_io.py
@@ -4,14 +4,26 @@
 import librosa
 import json
 
-def read_wav_file(folder: str):
-    path = "../data/" + folder
-    # TODO: read file
-    #return fs, y
+def read_wav_file(path: str):
+    y, sr = librosa.load(path, sr=None)
+    return sr, y
 
 def serialize_data():
     #TODO: Pickle
     pass
+
+def get_all_file_names(folder):
+    json_path = "../data/file_descriptions.json"
+    all_files = []
+    with open(json_path, "r", encoding="utf-8") as file:
+        data = json.load(file)
+        for item in data:
+            if "location" in item:
+                # Check if the file is test/train/validation
+                if item['location'].split("/")[0] == folder:
+                    file_loc = "../data/" + item['location']
+                    all_files.append(file_loc)
+    return all_files
 
 def get_all_chords():
     json_path = "../data/file_descriptions.json"
--- model/process_files.py
+++ model/process_files.py
@@ -3,9 +3,17 @@
 # Calculate the Constant Q spectrogram of that winodw. This spectrogram is
 # the feature, the chord is the label.
 
-def calculate_const_Q():
+import librosa.display
+import matplotlib.pyplot as plt
+import numpy as np
+
+from file_io import get_all_file_names, read_wav_file
+
+def calculate_const_Q(y, sr):
     # TODO: Laske constant Q spectrogrammi
-    pass
+    cqt = np.abs(librosa.cqt(y, sr=sr, hop_length=512, bins_per_octave=24, n_bins=84))
+    cqt_db = librosa.amplitude_to_db(cqt, ref=np.max)
+    return cqt_db
 
 def calculate_mel_spectro():
     # TODO: laske mel spectrogrammi, tarvitaanko?
@@ -14,4 +22,15 @@ def calculate_mel_spectro():
 def process_data():
     # TODO: splittaa lyhyihin ikkunoihin, laske spectrogrammi
     # Asenna label tälle ikkunnalle.
-    pass
+    train_file_names = get_all_file_names('train')
+    for idx, train_file in enumerate(train_file_names):
+        sr, y = read_wav_file(train_file)
+        constq = calculate_const_Q(y, sr)
+        plt.figure(figsize=(10, 4))
+        librosa.display.specshow(constq, sr=sr, x_axis="time", y_axis="cqt_note")
+        plt.colorbar(label="dB")
+        plt.title("Constant-Q Transform (CQT) Spectrogram")
+        plt.show()
+        break
+
+process_data()
