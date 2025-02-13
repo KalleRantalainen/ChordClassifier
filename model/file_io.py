668d9ede2d1b556023f928e0deadd9261e94cdd7
Kalle Rantalainen <kllrantalainen366@gmail.com>
2025-02-13 17:30:47 +0200

    Added some feature calculation stuff

--- model/file_io.py
+++ model/file_io.py
@@ -8,11 +8,39 @@ def read_wav_file(path: str):
     y, sr = librosa.load(path, sr=None)
     return sr, y
 
+
 def serialize_data():
     #TODO: Pickle
     pass
 
+def get_chords_and_times(file_name):
+    """
+    Get all the chords and chord timestaps for a song from the json file.
+    """
+    json_file_loc = "../data/file_descriptions.json"
+
+    # Load the JSON data
+    with open(json_file_loc, "r") as f:
+        data = json.load(f)
+
+    # Loop through the data to find the matching file
+    for entry in data:
+        if entry.get("location") == file_name:
+            # Extract chords and chord times
+            chords = entry.get("chords", [])
+            chords_time = entry.get("chords_time", [])
+            return chords, chords_time
+
+    # If not found, return None
+    print(f"File '{file_name}' not found in JSON.")
+    return None, None
+
+
 def get_all_file_names(folder):
+    """
+    Get all file names from the data split defined by folder.
+    So from test, train or validation
+    """
     json_path = "../data/file_descriptions.json"
     all_files = []
     with open(json_path, "r", encoding="utf-8") as file:
@@ -25,9 +53,16 @@ def get_all_file_names(folder):
                     all_files.append(file_loc)
     return all_files
 
+
 def get_all_chords():
+    """
+    Get all chords present in the data.
+    """
     json_path = "../data/file_descriptions.json"
     all_chords = []
+    train_chords = []
+    test_chords = []
+    validation_chords = []
     with open(json_path, "r", encoding="utf-8") as file:
         data = json.load(file)
         # Loop through the json dicts
@@ -35,7 +70,55 @@ def get_all_chords():
             # Check if 'chords' key is in the dict
             if "chords" in item:
                 # Get all the chords and append the ones we don't yet have
+                if item['location'].split("/")[0] == "train":
+                    for chord in item['chords']:
+                        if chord not in train_chords:
+                            train_chords.append(chord)
+                elif item['location'].split("/")[0] == "test":
+                    for chord in item['chords']:
+                        if chord not in test_chords:
+                            test_chords.append(chord)
+                elif item['location'].split("/")[0] == "validation":
+                    for chord in item['chords']:
+                        if chord not in validation_chords:
+                            validation_chords.append(chord)
                 for chord in item['chords']:
                     if chord not in all_chords:
                         all_chords.append(chord)
-    return all_chords
+    return all_chords, train_chords, test_chords, validation_chords
+
+
+def get_common_chords():
+    """
+    Get chords that are present in all of the data splits.
+    """
+    _, train_chords, test_chords, validation_chords = get_all_chords()
+
+    val_chords_in_train = []
+    for val_ch in validation_chords:
+        if val_ch in train_chords:
+            val_chords_in_train.append(val_ch)
+
+    common_chords = []
+    for test_ch in test_chords:
+        if test_ch in val_chords_in_train:
+            common_chords.append(test_ch)
+
+    return common_chords
+
+# For testing
+def main():
+
+    # Get all chords in the data + chords in each data split
+    all_chords, train_chords, test_chords, validation_chords = get_all_chords()
+    # Get chords that all present in every data split
+    common_chords = get_common_chords()
+
+    print("len all", len(all_chords))
+    print("len train", len(train_chords))
+    print("len test", len(test_chords))
+    print("len validation", len(validation_chords))
+    print("len common chords", len(common_chords))
+
+if __name__ == "__main__":
+    main()
--- model/process_files.py
+++ model/process_files.py
@@ -7,25 +7,125 @@ import librosa.display
 import matplotlib.pyplot as plt
 import numpy as np
 
-from file_io import get_all_file_names, read_wav_file
+from file_io import get_all_file_names, read_wav_file, get_chords_and_times
 
 def calculate_const_Q(y, sr):
-    # TODO: Laske constant Q spectrogrammi
-    cqt = np.abs(librosa.cqt(y, sr=sr, hop_length=512, bins_per_octave=24, n_bins=84))
+    cqt = np.abs(librosa.cqt(y, sr=sr, hop_length=256, bins_per_octave=24, n_bins=84))
     cqt_db = librosa.amplitude_to_db(cqt, ref=np.max)
     return cqt_db
+
+def one_hot_encode_target(chord, all_chords):
+    # Find the index of the chord in the all_chords list
+    chord_index = all_chords.index(chord)
+    # Vector of zeros
+    one_hot_vector = np.zeros(len(all_chords), dtype=int)
+    # Set the chord's value to 1
+    one_hot_vector[chord_index] = 1
+    return one_hot_vector
 
 def calculate_mel_spectro():
     # TODO: laske mel spectrogrammi, tarvitaanko?
     pass
 
-def process_data():
-    # TODO: splittaa lyhyihin ikkunoihin, laske spectrogrammi
-    # Asenna label tälle ikkunnalle.
-    train_file_names = get_all_file_names('train')
-    for idx, train_file in enumerate(train_file_names):
-        sr, y = read_wav_file(train_file)
+def calculate_features(qspec, y, sr, file_name):
+    # Number of bins for each feature. 6 bins corresponds to approx 100ms
+    number_of_bins = 6
+    # Total number of bins in the spectrogram
+    qspec_bins = qspec.shape[1]
+    # Calculate how many feature/target pairs can be formed from this song.
+    num_of_features = qspec_bins // number_of_bins
+    # file length in seconds
+    y_len_seconds = len(y) / sr
+    # How long each bin is in seconds
+    bin_len_seconds = y_len_seconds / qspec_bins
+
+    # Convert the name from "../data/test/name.wav" to "test/name.wav"
+    json_format_file_name = file_name.replace("../data/", "")
+    # Get the chords and their timestamps from the file
+    chords, chord_times = get_chords_and_times(json_format_file_name)
+    # Add start and end to the chord times
+    chord_times.insert(0, 0)
+    chord_times.append(y_len_seconds)
+    # Add None to the start and end of the chords
+    chords(0, None)
+    chords.append(None)
+
+    # If the song has signle E chord, the format is now for example
+    # chords = [None, E, None]
+    # chord_times = [0, 0.5, 10]
+    # So the is nothing from 0 to 0.5 seconds, then there is E from 0.5 to 10 seconds
+    # and nothing after that since the song has ended
+
+    if chords == None or chord_times == None:
+        return
+
+    # Loop through the bins and store feature/target pairs
+    for i in range(num_of_features - 1):
+        # Start and end idx for the current bins
+        start_idx = i * number_of_bins
+        end_idx = (i + 1) * number_of_bins
+        # Start and end times for the current bins
+        start_sec = i * bin_len_seconds
+        end_sec = i * bin_len_seconds
+        # Exract current bins from the spectrogram
+        current_bins = qspec[start_idx:end_idx]
+
+        # Placeholder for the active chord
+        active_chord = None
+        for j in range(len(chord_times) - 1):
+            # Check if the current window has only one chord
+            if chord_times[j] <= start_sec and chord_times[j + 1] >= end_sec:
+                # Single chord active in this window
+                active_chord = chords[j]
+            # There are two chords present during this window
+            else:
+                # Overlap starts when the current chord ends
+                overlap_start = chord_times[j + 1]
+                # Overlap ends when the current window ends
+                overlap_end = end_sec
+                # Calculate the overlap duration
+                overlap_duration = overlap_end - overlap_start
+                # If overlap takes more than half of the window's duration, then
+                # the overlapping chord is the active one.
+                if overlap_duration > (end_sec - start_sec) / 2:
+                    active_chord = chords[j + 1]
+                else:
+                    # If the overlap is less than half of the window duration, the active chord
+                    # is the current chord
+                    active_chord = chords[j]
+            # Active chord found, break the loop
+            break
+
+        # Handle case where no chord is active
+        if active_chord is None:
+            # Do not write any features
+            pass
+        else:
+            # Active chord was found -> create feature/target pair file.
+            pass
+
+
+
+
+def process_data(split):
+    """
+    Calculate the feaures from the data.
+    1. Calculate the a spectrogram for a whole song.
+    2. Split the spectrogram into short windows
+    3. Repeat for every file
+    """
+    file_names = get_all_file_names(split)
+    for idx, file_name in enumerate(file_names):
+        sr, y = read_wav_file(file_name)
+        if sr != 16000:
+            continue
         constq = calculate_const_Q(y, sr)
+        bins = constq.shape[1]
+        samples_per_bin = len(y) / bins
+        print(file_name)
+        print("sr:", sr)
+        print("constQ shape:", constq.shape)
+        print("Length of one bin:", samples_per_bin/sr*6, "s")
         plt.figure(figsize=(10, 4))
         librosa.display.specshow(constq, sr=sr, x_axis="time", y_axis="cqt_note")
         plt.colorbar(label="dB")
@@ -33,4 +133,10 @@ def process_data():
         plt.show()
         break
 
-process_data()
+# For testing
+def main():
+
+    process_data("test")
+
+if __name__ == "__main__":
+    main()
