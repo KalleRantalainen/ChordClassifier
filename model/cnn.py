ca192e2c621ea96e2a578680b1b5fb8b358bb052
Kalle Rantalainen <kllrantalainen366@gmail.com>
2025-02-25 14:18:25 +0200

    Implemented training loop

--- model/cnn.py
+++ model/cnn.py
@@ -8,7 +8,7 @@ class ChordCNN(nn.Module):
 
         # Block 1
         self.block_1 = Sequential(
-            Conv2d(in_channels=6, out_channels=32, kernel_size=(3,3), padding=1),
+            Conv2d(in_channels=1, out_channels=32, kernel_size=(3,3), padding=1),
             ReLU(),
             BatchNorm2d(32),
             MaxPool2d(kernel_size=2, stride=2),
@@ -40,21 +40,33 @@ class ChordCNN(nn.Module):
             Dropout2d(p=0.2)
         )
 
-        self.fc1 = Linear(256 * 5 * 5, 512)
+        self.fc1 = Linear(5376, 512)
         self.fc2 = Linear(512, num_chords)
 
         self.dropout = Dropout(p=0.5)
 
     def forward(self, x):
+        # Data is in shape (batch_size, freq_bands, time_steps), the 
+        # cnn expects shape (channels, batch_size, freq_bands, time_steps)
+        # -> need to unsqueeze before passing the data
+        #print("X shape before:", x.shape)
+        x = x.unsqueeze(1)
+        #print("X shape after unsqueeze:", x.shape)
         x = self.block_1(x)
+        #print("X shape after block 1:", x.shape)
         x = self.block_2(x)
+        #print("X shape after block 2:", x.shape)
         x = self.block_3(x)
+        #print("X shape after block 3:", x.shape)
         x = self.block_4(x)
+        #print("X shape after block 4:", x.shape)
 
         x = x.view(x.size(0), -1)
+        #print("X after reshape:", x.shape)
 
         x = F.relu(self.fc1(x))
         x = self.dropout(x)
         x = self.fc2(x)
+        #print("X after fc:", x.shape)
 
         return x
---
+++ model/train_model.py
@@ -0,0 +1,195 @@
+import torch
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+import numpy as np
+from copy import deepcopy
+
+from dataset import ChordDataset
+from cnn import ChordCNN
+
+def train(train_ds, test_ds, val_ds):
+    # Define the device
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print(f'Process on {device}', end='\n\n')
+
+    # Define some hyper params for training
+    epochs = 10
+    learning_rate = 1e-3
+    batch_size = 32
+
+    # Define the CNN model
+    model = ChordCNN()
+
+    # Pass model to the available device.
+    model = model.to(device)
+
+    # Define the optimizer and give the parameters of the CNN model to an optimizer.
+    optimizer = Adam(params=model.parameters(), lr=learning_rate)
+
+    # Using CrossEntropyLoss
+    loss_function = torch.nn.CrossEntropyLoss()
+
+    # Define the data loaders
+    train_loader = DataLoader(dataset=train_ds, batch_size=batch_size, shuffle=True)
+    validation_loader = DataLoader(dataset=val_ds, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(dataset=val_ds, batch_size=batch_size, shuffle=True)
+
+    # Define early stopping parameters
+    lowest_validation_loss = 1e10
+    best_validation_epoch = 0
+    patience = 5
+    patience_counter = 0
+    best_model = None
+
+    for epoch in range(epochs):
+
+        # Lists to hold the corresponding losses of each epoch.
+        epoch_loss_training = []
+        epoch_loss_validation = []
+
+        # Indicate that we are in training mode, so (e.g.) dropout
+        # will function
+        model.train()
+
+        print(f"\nEpoch {epoch+1}/{epochs}")
+        train_loader_tqdm = tqdm(train_loader, desc=f"Training Epoch {epoch+1}")
+
+        # For each batch of our dataset.
+        for batch in train_loader_tqdm:
+            # Zero the gradient of the optimizer.
+            optimizer.zero_grad()
+
+            # Get the batches.
+            x, y = batch
+
+            # Give them to the appropriate device.
+            x = x.to(device)
+            y = y.to(device).float() # Convert to float format -> cross entropy wants this
+
+            # Get the predictions.
+            y_hat = model(x)
+
+            # Calculate the loss .
+            loss = loss_function(y_hat, y)
+
+            # Do the backward pass
+            loss.backward()
+
+            # Do an update of the weights (i.e. a step of the optimizer)
+            optimizer.step()
+
+            # Append the loss of the batch
+            epoch_loss_training.append(loss.item())
+
+            # Update progress bar
+            train_loader_tqdm.set_postfix(loss=loss.item())
+
+        # Indicate that we are in evaluation mode
+        model.eval()
+
+        valid_loader_tqdm = tqdm(validation_loader, desc=f"Validating Epoch {epoch+1}")
+
+        # Evaluate the model using validation data
+        with torch.no_grad():
+            # For every batch of our validation data.
+            for batch in valid_loader_tqdm:
+                # Get the batch
+                x_val, y_val = batch
+
+                # Pass the data to the appropriate device.
+                x_val = x_val.to(device)
+                y_val = y_val.to(device).float()
+
+                # Get the predictions of the model.
+                y_hat = model(x_val)
+
+                # Calculate the loss.
+                loss = loss_function(y_hat, y_val)
+
+                # Append the validation loss.
+                epoch_loss_validation.append(loss.item())
+                # Update the progress bar
+                valid_loader_tqdm.set_postfix(loss=loss.item())
+
+        # Calculate mean losses.
+        epoch_loss_validation = np.array(epoch_loss_validation).mean()
+        epoch_loss_training = np.array(epoch_loss_training).mean()
+        print(f"Epoch {epoch} training loss  : {epoch_loss_training:.2f}")
+        print(f"Epoch {epoch} validation loss: {epoch_loss_validation:.2f}")
+
+        # Check early stopping conditions.
+        if epoch_loss_validation < lowest_validation_loss:
+            lowest_validation_loss = epoch_loss_validation
+            patience_counter = 0
+            best_model = deepcopy(model.state_dict())
+            best_validation_epoch = epoch
+        else:
+            patience_counter += 1
+
+        # If we have to stop, do the testing.
+        if patience_counter >= patience or epoch == epochs - 1:
+            print('\nExiting due to early stopping', end='\n\n')
+            print(f'Best epoch {best_validation_epoch} with loss {lowest_validation_loss}', end='\n\n')
+            if best_model is None:
+                print('No best model. ')
+            else:
+                # Save and load the best model.
+                torch.save(best_model, 'best_model.pt')
+                model.load_state_dict(best_model)
+
+                # Process similar to validation.
+                print('Starting testing', end=' | ')
+                testing_loss = []
+                labels_test = []
+                predictions_test = []
+                model.eval()
+                with torch.no_grad():
+                    for batch in test_loader:
+                        x_test, y_test = batch
+
+                        # Pass the data to the appropriate device.
+                        x_test = x_test.to(device)
+                        y_test = y_test.to(device).float()
+
+                        # make the prediction
+                        y_hat = model(x_test)
+
+                        # Calculate the loss.
+                        loss = loss_function(y_hat, y_test)
+                        testing_loss.append(loss.item())
+
+                        # Save the predictions and labels for later analysis.
+                        predictions_test.append(y_hat.argmax(dim=-1).cpu().numpy())
+                        labels_test.append(y_test.argmax(dim=-1).cpu().numpy())
+
+                testing_loss = np.array(testing_loss).mean()
+                print(f'Testing loss: {testing_loss:7.4f}')
+
+                # Calculate the accuracy.
+                predictions_test = np.concatenate(predictions_test)
+                labels_test = np.concatenate(labels_test)
+
+                # Calculate the amount of correct predictions
+                correct_predictions = np.equal(predictions_test, labels_test)
+                accuracy = np.mean(correct_predictions)
+                print("Amount of correct predictions:", np.sum(correct_predictions))
+                print("Test accuracy:", accuracy)
+                break
+
+# Train the model
+def main():
+    
+    root = "../data"
+    train_split = "/train_serialized"
+    test_split = "/test_serialized"
+    validation_split = "/validation_serialized"
+
+    train_dataset = ChordDataset(root, train_split)
+    test_dataset = ChordDataset(root, test_split)
+    validation_dataset = ChordDataset(root, validation_split)
+
+    train(train_dataset, test_dataset, validation_dataset)
+    
+if __name__ == "__main__":
+    main()
